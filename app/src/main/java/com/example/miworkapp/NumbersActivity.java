ad2f5e2967823d7d5db5ae0a3efabe90b1354cf6
swapan <swapan.d83@gmail.com>
2020-02-28 23:18:54 +0700

    Used for loop instead of while loop

---
+++ app/src/main/java/com/example/miworkapp/NumbersActivity.java
@@ -0,0 +1,60 @@
+package com.example.miworkapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+        // Create a list of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Verify the contents of the list by printing out each element to the logs
+        /*
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
+        */
+        // Find the root view so we can add child views to it
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+
+        // Keep looping until we've reached the end of the list (which means keep looping
+        // as long as the current index position is less than the length of the list).
+        // The index variable keeps track of our current position in the list and
+        // increments by 1 each time the code in the loop is executed.
+        for (int index = 0; index < words.size();index++){
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+
+           // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+
+        }
+
+    }
+}
